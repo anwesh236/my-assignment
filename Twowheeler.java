48491e73c81e66ef84658eda9562911fc3cd2abb
anwesh236 <119473282+anwesh236@users.noreply.github.com>
2022-11-30 10:51:10 +0530

    Add files via upload

---
+++ Twowheeler.java
@@ -0,0 +1,36 @@
+package day2;
+
+public class Twowheeler {
+	int noOfWheels=2;
+	short noOfMirrors=2;
+	long chasisNumber=987654325034136l;
+	boolean ispunctured=true;
+    double runningKM=23.87;
+public static void main(String[] args) {
+	Twowheeler bike=new Twowheeler();
+	System.out.println(" noOfWheels"+bike.noOfWheels);
+	System.out.println("noOfMirrors"+bike.noOfMirrors);
+	System.out.println("chasisNumber"+bike.chasisNumber);
+	System.out.println("ispunctured"+bike.ispunctured);
+	System.out.println(" runningKM"+bike.runningKM);
+	
+	
+	
+
+	}
+			
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
